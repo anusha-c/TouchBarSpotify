54690c35c70ebb1c3a8d2b490967862faf1d3b2a
Anusha Choudhary <myselfshares@gmail.com>
2020-10-08 21:25:04 +0530

    Initial Commit

---
+++ TouchBarSpotify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarSpotify
+//
+//  Created by Anusha Choudhary on 10/8/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarSpotify/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TouchBarSpotify
+//
+//  Created by Anusha Choudhary on 10/8/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
